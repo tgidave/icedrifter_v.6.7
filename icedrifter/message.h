ec02b2b0746306ed6108291de61466d711c21f2e
Dave Eldenburg <dave@tgi.com>
2024-08-01 08:56:22 -0600

    More changes...

--- icedrifter/message.h
+++ icedrifter/message.h
@@ -10,11 +10,14 @@
 #define MSG_RX 3
 #define MSG_TX 2
 #define MSG_BAUD  9600
+
 // The DROP_MSG_RX_TX define tells the code to turn the TX and RX 
 // line off to keep any voltage leakage to a minimum.
 #define DROP_MSG_RX_TX
 
-#define MAX_TRIES 500
+#define MAX_XFER_TRIES  5
+
+#define MAX_TRIES       500
 
 bool processMessage(uint8_t *messageBuffPtr);
 
